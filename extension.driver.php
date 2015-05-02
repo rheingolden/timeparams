4678c46e1b787c5f04896f1f6876b053eeada0d1
Kyle McGuire <kyle@kymcism.com>
2015-05-02 10:17:33 -0700

    Adding relative dates and unix time stamp

--- extension.driver.php
+++ extension.driver.php
@@ -26,7 +26,11 @@ class extension_timeparams extends Extension {
 
 	public function addParam(&$context) {
 		$context['params']['next-year'] = date('Y')+1;
+		$context['params']['unix-now'] = time();
 		$context['params']['last-year'] = date('Y')-1;
+		$context['params']['tomorrow'] = substr(date('c', time()+24*60*60), 0, 10);
+		$context['params']['today-plus-3'] = substr(date('c', time()+3*24*60*60), 0, 10);
+		$context['params']['today-minus-3'] = substr(date('c', time()-3*24*60*60), 0, 10);
 	}
 
 }
